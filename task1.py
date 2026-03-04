e3bf74634d21ea679ddd34243a06bf88fde84759
Fabiha Chowdhury <auw242094@auw.edu.bd>
2026-03-04 20:48:58 +0600

    task1.py

--- task1.py
+++ task1.py
@@ -1,19 +1,19 @@
 # A
-file = open("student info.txt", "w")
+file = open("student_info.txt", "w")
 file.write("C12\n")
 file.write("C23\n")
 file.write("C44\n")
 file.close()
 
 # B
-file = open("student info.txt", "w")
+file = open("student_info.txt", "a")
 file.write("C13\n")
 file.write("C24\n")
 file.write("C45\n")
 file.close()
 
 # C
-file = open("student info.txt", "r")
+file = open("student_info.txt", "r")
 for line in file:
     print(line.strip())
 file.close()
